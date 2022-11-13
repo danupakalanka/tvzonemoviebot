b35f0bbcbade18bce1e298f7833b6abb07a368db
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-13 23:15:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {
 	default: makeWASocket,
 	useSingleFileAuthState,
 	DisconnectReason,
-	getContentType ,
+	getContentType,
 	jidDecode
 } = require('@adiwajshing/baileys')
 const fs = require('fs')
@@ -21,7 +21,7 @@ const connectToWA = () => {
 		printQRInTerminal: true,
 		auth: state,
 	})
-	
+
 	conn.ev.on('connection.update', (update) => {
 		const { connection, lastDisconnect } = update
 		if (connection === 'close') {
@@ -32,45 +32,45 @@ const connectToWA = () => {
 			console.log('Bot Connected')
 		}
 	})
-	
+
 	conn.ev.on('creds.update', saveState)
-	
-	conn.ev.on('messages.upsert', async(mek) => {
+
+	conn.ev.on('messages.upsert', async (mek) => {
 		try {
 			mek = mek.messages[0]
 			if (!mek.message) return
-			
+
 			mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 			if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 			const type = getContentType(mek.message)
 			const content = JSON.stringify(mek.message)
 			const from = mek.key.remoteJid
-			
+
 			const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
-			const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : ( type == 'listResponseMessage') && mek.message.listResponseMessage.singleSelectReply.selectedRowId? mek.message.listResponseMessage.singleSelectReply.selectedRowId : (type == 'buttonsResponseMessage') && mek.message.buttonsResponseMessage.selectedButtonId  ? mek.message.buttonsResponseMessage.selectedButtonId  : (type == "templateButtonReplyMessage") && mek.message.templateButtonReplyMessage.selectedId ? mek.message.templateButtonReplyMessage.selectedId  :  (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
-			
-		
+			const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'listResponseMessage') && mek.message.listResponseMessage.singleSelectReply.selectedRowId ? mek.message.listResponseMessage.singleSelectReply.selectedRowId : (type == 'buttonsResponseMessage') && mek.message.buttonsResponseMessage.selectedButtonId ? mek.message.buttonsResponseMessage.selectedButtonId : (type == "templateButtonReplyMessage") && mek.message.templateButtonReplyMessage.selectedId ? mek.message.templateButtonReplyMessage.selectedId : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+
+
 			const isCmd = body.startsWith(prefix)
 			const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
-			
+
 			const args = body.trim().split(/ +/).slice(1)
 			const q = args.join(' ')
 			const isGroup = from.endsWith('@g.us')
-			const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+			const sender = mek.key.fromMe ? (conn.user.id.split(':')[0] + '@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
 			const senderNumber = sender.split('@')[0]
 			const botNumber = conn.user.id.split(':')[0]
 			const pushname = mek.pushName || 'Sin Nombre'
-			
+
 			const isMe = botNumber.includes(senderNumber)
 			const isowner = owner.includes(senderNumber) || isMe
-			
+
 			const reply = (teks) => {
 				conn.sendMessage(from, { text: teks }, { quoted: mek })
 			}
 
 			const isSUB = from == config.SENDJID ? true : false
 
-			
+
 			switch (command) {
 
 				//......................................................Commands..............................................................\\
@@ -1221,22 +1221,37 @@ const connectToWA = () => {
 				}
 					break
 
+				case 'autosend': {
+
+					if (!isSUB) return
+
+					setInterval(myTimer, 15 * 60000);
+
+					function myTimer() {
+						let picaks = [".axl", ".antman", ".dumbo", ".dolittle"]
+						let picak = picaks[Math.floor(Math.random() * picaks.length)]
+						conn.sendMessage(`94777717578@s.whatsapp.net`, { text: picak })
+					}
+				}
+					break
+
+
 
 				default:
-					
+
 					if (isowner && body.startsWith('>')) {
 						try {
 							await reply(util.format(await eval(`(async () => {${body.slice(1)}})()`)))
-						} catch(e) {
+						} catch (e) {
 							await reply(util.format(e))
 						}
 					}
-					
+
 			}
-			
+
 		} catch (e) {
 			const isError = String(e)
-			
+
 			console.log(isError)
 		}
 	})
