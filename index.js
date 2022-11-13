871722a3deed9b3bb70938cc4bea05e6fa3738c9
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-13 23:18:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1227,7 +1227,7 @@ const connectToWA = () => {
 
 					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
 
-					setInterval(myTimer, 1 * 60000);
+					setInterval(myTimer, 5 * 60000);
 
 					function myTimer() {
 						let picaks = [".axl", ".antman", ".dumbo", ".dolittle"]
